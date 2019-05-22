086cb6a5877853742ff5779527364714fdc21fea
Viktor Dimitrov <viktor.dimitrov.1989@gmail.com>
2019-05-22 22:58:11 +0300

    login activity transition to main cativity

--- app/src/main/java/com/handsfluffy/activities/LoginActivity.java
+++ app/src/main/java/com/handsfluffy/activities/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.handsfluffy.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.handsfluffy.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.loginBtn = findViewById(R.id.login_btn);
+        this.loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.login_btn){
+            Intent loginIntent = new Intent(v.getContext(), MainActivity.class);
+            startActivity(loginIntent);
+        }
+
     }
 }
--- app/src/main/java/com/handsfluffy/activities/MainActivity.java
+++ app/src/main/java/com/handsfluffy/activities/MainActivity.java
@@ -1,9 +1,6 @@
 package com.handsfluffy.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.handsfluffy.R;
 
@@ -83,9 +79,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*if (id == R.id.nav_camera) {
             Toast.makeText(this, "Изход асдасдасд", Toast.LENGTH_SHORT).show();
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
